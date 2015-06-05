240c19c498ed3829c6cfe0c032be484609d22ba1
Che-Huai Lin <lzh9102@gmail.com>
2015-06-06 00:50:53 +0800

    Set application organization name

--- main.cpp
+++ main.cpp
@@ -5,6 +5,9 @@ int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
+    // QSettings will save the config file in $XDG_CONFIG_HOME/<organization>/<program>.conf
+    app.setOrganizationName("dictation_helper");
+
     MainWindow w;
     w.show();
 
